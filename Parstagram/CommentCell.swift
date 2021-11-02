be95d37afb253c3d42ab160feed1f9efd63e59f5
menukaghalan <59702166+menukaghalan@users.noreply.github.com>
2021-11-01 23:56:13 -0400

    Commit
    Commit

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,31 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Menuka Ghalan on 11/1/21.
+//  Copyright © 2021 Menuka Ghalan. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'Parstagram' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   pod 'Parse'
+  pod "AlamofireImage"
+  pod "MessageInputBar"
 
   # Pods for Parstagram
 
